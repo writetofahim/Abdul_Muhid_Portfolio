3319adc32408f70920ac3d4579718f2bdd6fbcd7
writetofahim <writetofahimahammad@gmail.com>
2023-06-16 22:40:49 +0600

    nav done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="bg-secondary-500  w-full z-20 top-0 left-0">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <nav className=" bg-secondary-500  w-full z-20 top-0 left-0">
+      <div className="container flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://flowbite.com/" className="flex items-center">
           
           <span className="self-center text-2xl font-semibold whitespace-nowrap">Abdul Muhid</span>
